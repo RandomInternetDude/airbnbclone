1081b5d12dd94db15234948fb81cf8177816feae
Joey <joey7040@hotmail.com>
2020-11-24 19:30:12 -0500

    created header and home component

---
+++ src/Components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../Css/Home.css'
+
+export default function Home() {
+    return (
+        <div className='home'>
+            <h2>home component</h2>
+        </div>
+    )
+}
