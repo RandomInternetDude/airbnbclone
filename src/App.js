e2e417de035d723938327ca89d4b1076f2dcd818
Joey <joey7040@hotmail.com>
2020-11-27 17:41:30 -0500

    added footer and search page, as well as links to search page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 // import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from './Components/Home';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
+import SearchPage from './Components/SearchPage';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      
+      <Router>
+
+        <Header/>
+        
+        <Switch>
+            <Route path="/search">
+              <SearchPage />
+            </Route>
+            <Route path="/">
+              <Home/>
+            </Route>
+
+        </Switch>
+        
+        
+        <Footer />
+      </Router>
     </div>
     
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,12 +10,12 @@ import {Avatar} from '@material-ui/core';
 function Header() {
     return (<div className='header'>
    
-                <img
+                <Link to='/'><img
                     className="header__icon"
                     src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
                     alt=""
                 />
-
+                </Link>
                 <div className='header__center'> 
                     <input type='text' />
                     <SearchIcon />
