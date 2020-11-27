dd1f954bfe44b3244d7111b8cd8357a7d9a5c4aa
Joey <joey7040@hotmail.com>
2020-11-26 20:27:00 -0500

    built out cards finished date picker component

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -3,10 +3,13 @@ import '../Css/Search.css'
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 import {DateRangePicker} from 'react-date-range';
+import PeopleIcon from "@material-ui/icons/People";
+import {Button} from "@material-ui/core";
+import {useHistory} from "react-router-dom";
 
 
-
-export default function Search(){
+function Search() {
+    const history = useHistory();
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
 
@@ -14,14 +17,23 @@ export default function Search(){
         startDate: startDate,
         endDate: endDate,
         key: "selection",
-    };
+      };
 
-    function handleSelect(ranges){
+    function handleSelect(ranges) {
         setStartDate(ranges.selection.startDate);
-        setEndDate(ranges.selection.endDate);    
+        setEndDate(ranges.selection.endDate);
     }
 
-    return(<div className='search'>
-        <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
-    </div>)
+    return (
+        <div className='search'>
+            <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
+            <h2>
+                Number of guests <PeopleIcon />
+            </h2>
+            <input min={0} defaultValue={2} type="number" />
+            <Button onClick={() => history.push('/search')}>Search Airbnb</Button>
+        </div>
+    )
 }
+
+export default Search
